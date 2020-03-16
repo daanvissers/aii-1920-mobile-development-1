5f1a1aed464c0d0b47c6fa6891287486ee39e681
Daan Vissers <daanvissers81@gmail.com>
2020-03-16 16:50:23 +0100

    Add infinite scrolling

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 import { PokemonService } from '../services/pokemon.service';
 
 @Component({
@@ -11,17 +12,32 @@ export class HomePage implements OnInit {
   pokemon = [];
   offset = 0;
 
+  @ViewChild(IonInfiniteScroll, null) infiniteScroll: IonInfiniteScroll;
+
   constructor(private pokemonService: PokemonService) {}
 
   ngOnInit() {
     this.loadPokemon();
   }
 
-  loadPokemon() {
+  loadPokemon(loadMore = false, event?) {
+
+    // Load another 25 items
+    if (loadMore)
+      this.offset += 25;
+
     this.pokemonService.getPokemon(this.offset)
       .subscribe(res => {
         console.log('result: ', res);
-        this.pokemon = res;
+        // Append new Pokemon to existing array
+        this.pokemon = [...this.pokemon, ...res];
+
+        if (event)
+          event.target.complete();
+
+        if (this.offset == 125) {
+          this.infiniteScroll.disabled = true;
+        }
       });
   }
 
